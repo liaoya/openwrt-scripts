68802bb4b78fcfa69fc344dd9f194f1ed5958a63
Huifeng Shen <liaoya@gmail.com>
2018-12-03 22:56:46 +0800

    Init Import

---
+++ k2p/README.md
@@ -0,0 +1,5 @@
+# Build K2P
+
+## Preparation
+
+`apt install -y ocaml-nox help2man texinfo yui-compressor`
---
+++ k2p/build.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#shellcheck disable=SC2164
+
+set -e -x
+
+CLEAN=0
+
+TEMP=$(getopt -o c:: --long clean:: -- "$@")
+eval set -- "$TEMP"
+while true ; do
+    case "$1" in
+        -c|--clean)
+            CLEAN=1 ; shift 2 ;;
+        --) shift ; break ;;
+        *) echo "Usage [-v|--version] <openwrt version> [-c|--clean]" ; exit 1 ;;
+    esac
+done
+
+PACKAGES="-wpad-mini -dnsmasq \
+ca-bundle ca-certificates coreutils-base64 curl \
+dnsmasq-full \
+file \
+ip-full ipset iptables-mod-tproxy \
+libpthread \
+luci luci-app-firewall luci-theme-bootstrap luci-app-adblock \
+luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn luci-i18n-adblock-zh-cn \
+uci \
+wpad \
+ChinaDNS luci-app-chinadns dns-forwarder luci-app-dns-forwarder shadowsocks-libev luci-app-shadowsocks simple-obfs ShadowVPN luci-app-shadowvpn \
+vlmcsd \
+"
+
+BASE_URL=https://downloads.openwrt.org/releases/18.06.1/targets/ramips/mt7621/
+
+curl -sLO "${BASE_URL}/sha256sums"
+SHA256_VALUE=$(grep imagebuilder sha256sums | cut -d' ' -f1)
+IMAGE_BUILDER_FILENAME=$(grep imagebuilder sha256sums | cut -d'*' -f2)
+if [[ -f "${IMAGE_BUILDER_FILENAME}" ]]; then
+    if [[ $(sha256sum "${IMAGE_BUILDER_FILENAME}" | cut -d' ' -f1) != "${SHA256_VALUE}" ]]; then
+        rm -f "${IMAGE_BUILDER_FILENAME}"
+    fi
+fi
+
+if [[ ! -f "${IMAGE_BUILDER_FILENAME}" ]]; then
+    curl -sLO "${BASE_URL}/${IMAGE_BUILDER_FILENAME}"
+fi
+
+tar -xf "${IMAGE_BUILDER_FILENAME}"
+#shellcheck disable=SC2046
+if [[ $CLEAN -gt 0 && -d $(basename -s .tar.xz "${IMAGE_BUILDER_FILENAME}") ]]; then rm -fr $(basename -s .tar.xz "${IMAGE_BUILDER_FILENAME}"); fi
+
+cd $(basename -s .tar.xz "${IMAGE_BUILDER_FILENAME}")
+for repo in "src/gz reboot_openwrt_dist http://openwrt-dist.sourceforge.net/packages/base/mipsel_24kc" \
+	    "src/gz reboot_openwrt_dist_luci http://openwrt-dist.sourceforge.net/packages/luci" \
+	    "src/gz reboot_vlmcsd http://cokebar.github.io/openwrt-vlmcsd/LEDE"; do
+    repo=$(echo "${repo}" | sed 's/\//\\\//g')
+    sed -i "/telephony$/a ${repo}" repositories.conf
+done
+
+[[ $CLEAN -gt 0 ]] && make clean
+make -j "$(nproc)" image PROFILE=k2p PACKAGES="${PACKAGES}" FILES=../custom EXTRA_IMAGE_NAME=custom
