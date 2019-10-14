49d5a8bdc962da8c74326bd93ebda8eb8e138d52
Huifeng Shen <liaoya@gmail.com>
2019-10-14 16:28:53 +0800

    Add v2ray

--- sdk/build.sh
+++ sdk/build.sh
@@ -59,9 +59,10 @@ if [[ ! -f "${CACHE_DIR}/${SDK_FILENAME}" ]]; then
     curl -sL "${BASE_URL}/${SDK_FILENAME}" -o "${CACHE_DIR}/${SDK_FILENAME}"
 fi
 SDK_DIR=$(basename -s .tar.xz "${SDK_FILENAME}")
-SDK_DIR=$(basename -s .tar.xz "${SDK_FILENAME}")
 if [[ ${CLEAN} -gt 0 && -d "${SDK_DIR}" ]]; then rm -fr "${SDK_DIR}"; fi
-if [[ ! -d $(basename -s .tar.xz "${SDK_FILENAME}") ]]; then tar -xf "${CACHE_DIR}/${SDK_FILENAME}"; fi
+if [[ ! -d "${SDK_DIR}" ]]; then tar -xf "${CACHE_DIR}/${SDK_FILENAME}"; fi
+
+if [[ $(command -v pre_ops) ]]; then pre_ops; fi
 
 if [[ -d "${ROOT_DIR}/lede" ]]; then
     (cd "${ROOT_DIR}/lede"; git fetch -p --all; git pull)
@@ -69,12 +70,22 @@ else
     git clone https://github.com/coolsnowwolf/lede.git
 fi
 
-if [[ $(command -v pre_ops) ]]; then pre_ops; fi
-
 if [[ -d "${SDK_DIR}/package/lean" ]]; then
     rm -fr "${SDK_DIR}/package/lean"
 fi
 cp -pr "${ROOT_DIR}/lede/package/lean" "${SDK_DIR}/package"
+
+if [[ -d "${SDK_DIR}/package/v2ray-core" ]]; then
+    (cd "${SDK_DIR}/package/v2ray-core"; git fetch -p --all; git pull)
+else
+    git clone https://github.com/kuoruan/openwrt-v2ray.git "${SDK_DIR}/package/v2ray-core"
+fi
+
+if [[ -d "${SDK_DIR}/package/luci-app-v2ray" ]]; then
+    (cd "${SDK_DIR}/package/luci-app-v2ray"; git fetch -p --all; git pull)
+else
+    git clone https://github.com/kuoruan/luci-app-v2ray.git "${SDK_DIR}/package/luci-app-v2ray"
+fi
 
 cd "${SDK_DIR}"
 ./scripts/feeds update -a
@@ -84,11 +95,12 @@ make -j"$(nproc)" package/feeds/luci/luci-base/compile
 make -j"$(nproc)" package/lean/luci-app-ssr-plus/compile
 make -j"$(nproc)" package/lean/shadowsocksr-libev/compile
 make -j"$(nproc)" package/lean/v2ray/compile
+make -j"$(nproc)" package/lean/pdnsd-alt/compile
 # Build adbyby plus
 make -j"$(nproc)" package/lean/adbyby/compile
 make -j"$(nproc)" package/lean/luci-app-adbyby-plus/compile
 # Build vlmcsd
-make -j"$(nproc)" make package/lean/luci-app-vlmcsd/compile
+make -j"$(nproc)" package/lean/luci-app-vlmcsd/compile
 make -j"$(nproc)" package/lean/vlmcsd/compile
 make package/index
 
