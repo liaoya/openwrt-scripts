218755cb1fbba9ecea4e0e65e72ffe8f46902cc8
Huifeng Shen <liaoya@gmail.com>
2020-04-10 00:28:00 +0800

    Add luci-app-wol

--- images/devices/psg1218a/custom.sh
+++ images/devices/psg1218a/custom.sh
@@ -11,10 +11,10 @@ if [[ -n ${BASE_URL_PREFIX} ]]; then
 fi
 
 PACKAGES=${PACKAGES:-""}
-PACKAGES="${PACKAGES:+$PACKAGES }luci luci-ssl luci-theme-bootstrap luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn luci-i18n-opkg-zh-cn"
-PACKAGES="${PACKAGES:+$PACKAGES }luci-app-vlmcsd luci-i18n-vlmcsd-zh-cn"
+PACKAGES="${PACKAGES:+$PACKAGES }luci luci-ssl luci-theme-bootstrap luci-app-wol luci-app-vlmcsd"
+# PACKAGES="${PACKAGES:+$PACKAGES }luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn luci-i18n-opkg-zh-cn luci-i18n-vlmcsd-zh-cn"
 if [[ ${VERSION} =~ 19.07 ]]; then
-    PACKAGES="${PACKAGES:+$PACKAGES } luci-compat luci-lib-ipkg uhttpd-mod-ubus"
+    PACKAGES="${PACKAGES:+$PACKAGES }luci-compat luci-lib-ipkg uhttpd-mod-ubus"
 fi
 
 pre_ops() {
