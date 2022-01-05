47f16c40621b8c6430d1009801a2ddbbcf3c883b
Huifeng Shen <liaoya@gmail.com>
2022-01-05 06:07:38 +0000

    Make squid cache work

--- squid/pre.sh
+++ squid/pre.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-add_image "docker.io/yaekee/squid:4.13"
-add_image "docker.io/yaekee/squid:latest"
+_this_dir=$(readlink -f "${BASH_SOURCE[0]}")
+_this_dir=$(dirname "${THIS_DIR}")
+
+for _item in "${_this_dir}"/*.sh; do
+    if [[ ! -x "${_item}" ]]; then
+        chmod a+x "${_item}"
+    fi
+done
+
+SQUID_VERSION=${SQUID_VERSION:-5.2-r0}
+_image_prefix=docker.io/yaekee
+
+add_image "${_image_prefix}/squid:${SQUID_VERSION}"
+add_image "${_image_prefix}/squid:latest"
