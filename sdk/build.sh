85ef2a7ebfc90bfd1e290b795789f490f9bea175
Tom Shen <tom.shen@calix.com>
2023-11-15 08:29:25 +0800

    identify slow package

--- sdk/build.sh
+++ sdk/build.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
+declare -a FAILURE_PACKAGES=(adguardhome airconnect alist amule-dlp ariang asterisk-chan-quectel cloudflared dns-proxy fibocom_QMI_WWAN frp homebox joker luci-app-adguardhome luci-app-airconnect luci-app-amule luci-app-aria2 luci-app-bypass luci-app-frpc luci-app-frps luci-app-homebox luci-app-kodexplorer luci-app-netspeedtest luci-app-nginx-manager luci-app-npc luci-app-nps luci-app-pcimodem luci-app-smartdns luci-app-spdmodem luci-app-speedtest-web luci-app-unblockmusic luci-app-usbmodem luci-app-webd luci-app-wifidog luci-app-xunyou)
+
 declare -a SLOW_PACKAGES=()
+SLOW_PACKAGES+=(ariang asterisk-chan-quectel cloudreve)
+SLOW_PACKAGES+=(libtorrent-rasterbar)
+SLOW_PACKAGES+=(luci-app-cd8021x luci-app-easymesh luci-app-music-remote-center luci-app-qbittorrent luci-app-qbittorrent-simple luci-app-rtorrent luci-app-squid)
 SLOW_PACKAGES+=(naiveproxy)
 SLOW_PACKAGES+=(trojan trojan-plus)
 
 function build_one_dir() {
-    local _build=1
+    local _build
     #shellcheck disable=SC2012
     while IFS= read -r pkg; do
+        _build=1
         pkg=$(basename "${pkg}")
+        for item in "${FAILURE_PACKAGES[@]}"; do
+            if [[ ${item} == "${pkg}" ]]; then
+                _build=0
+                break
+            fi
+        done
         for item in "${SLOW_PACKAGES[@]}"; do
             if [[ ${item} == "${pkg}" ]]; then
                 _build=0
@@ -21,12 +33,12 @@ function build_one_dir() {
         while IFS= read -r -d '' pkg_path; do
             start=$(date +%s)
             if ! make -j "${pkg_path}"/compile 2>/dev/null; then
-                echo "make V=sc ${pkg_path}/compile" >>fail.log
+                echo "make V=sc ${pkg_path}/compile" >>bin/fail.log
             else
-                echo "${pkg}" >>sucess.log
+                echo "${pkg}" >>bin/sucess.log
             fi
             end=$(date +%s)
-            echo "It took $((end - start)) seconds to build ${pkg}" >>bench.log
+            echo "It took $((end - start)) seconds to build ${pkg}" >>bin/bench.log
         done < <(find package -iname "${pkg}" -print0)
     done < <(ls -d "${1}"/*/ | sort)
 }
@@ -38,15 +50,9 @@ function build() {
 }
 
 for _file in bench.log fail.log sucess.log; do
-    if [[ -f ${_file} ]]; then
-        rm -f "${_file}"
+    if [[ -f "bin/${_file}" ]]; then
+        rm -f "bin/${_file}"
     fi
 done
 
 build
-
-for _file in bench.log fail.log sucess.log; do
-    if [[ -f ${_file} ]]; then
-        cp "${_file}" bin/
-    fi
-done
